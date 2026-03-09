63e71b7f2032350fab26f53e4bb215f3201fa911
Joi7e0 <easyfrog2002@gmail.com>
2026-03-09 20:50:20 +0100

    chore: setup eslint stylistic rules, add docs and fix first 8 formatting problems

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,11 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default Object.assign(defineConfig([
+  globalIgnores(['dist', 'node_modules', 'coverage', '.env*']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -13,6 +14,9 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -24,6 +28,11 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'always'],
+      '@stylistic/js/no-multiple-empty-lines': ['error', { max: 1 }],
+      '@stylistic/js/no-trailing-spaces': 'error',
     },
   },
-])
+]))
